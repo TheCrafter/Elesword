6e2f07cf8be22d50347be0bd31f1d60b5171910e
TheCrafter <dimitrisvlh@gmail.com>
2015-10-01 19:56:35 +0300

    Completed 'Hello Window' from learnopengl.com
     - Created OpenGl context
    
     - Added sample keyboard input handling
    
     - Added sample rendering

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,81 @@
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 #include <iostream>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+#ifdef _WIN32
+    //FreeConsole();
+#endif
 
+    std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+    // Init GLFW
+    glfwInit();
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if(window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetKeyCallback(window, keyCallback);
+    glfwMakeContextCurrent(window);
+
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if(glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Define the viewport dimensions
+    glViewport(0, 0, 800, 600);
+
+    // Game loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
     return 0;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    (void*)scancode;
+    (void*)mode;
+
+    // When a user presses the escape key, we set the WindowShouldClose property to true, 
+    // closing the application
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 }
